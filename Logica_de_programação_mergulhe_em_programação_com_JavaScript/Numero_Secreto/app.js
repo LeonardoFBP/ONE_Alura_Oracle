60e9d4a53f6800657bbefe7d951be1265ad21e6b
Leonardo Filipe <leonardo.filipe.trabalho.ti@gmail.com>
2025-01-22 21:24:04 -0300

    Alterando string do Alert para plural
    Notou -se que para Numerotentativa= 1, era exigibido 'Tentativas'. Dito isto foi removido do escopo do while-IF para fora do While, desta forma irá rodar o Alert apenas após verificado o valor de Numerotentativa.

--- Logica_de_programação_mergulhe_em_programação_com_JavaScript/Numero_Secreto/app.js
+++ Logica_de_programação_mergulhe_em_programação_com_JavaScript/Numero_Secreto/app.js
@@ -1,14 +1,11 @@
 let NumeroSecreto = 29;
 let tentativa 
-let Numerotentativa = 1   // Declarando a variavel p/ ao final quando der erro acrescentar mais 1.
+let Numerotentativa = 1   
 while (NumeroSecreto!= tentativa){
     tentativa = prompt('Escolha seu numero:');
 
     if (NumeroSecreto == tentativa) {
-        console.log('Muito bom, você acertou!! O número era ' + NumeroSecreto + '!!') 
-        
-        alert(`Muito bom, você acertou em ${Numerotentativa} tentativas!!`)
-        
+        console.log('Muito bom, você acertou!! O número era ' + NumeroSecreto + '!!')  
     }
     else{
         if(NumeroSecreto < tentativa){
@@ -17,7 +14,13 @@ while (NumeroSecreto!= tentativa){
         else{
             alert(`O número ${tentativa}, é menor que o número secreto!!`)
         }
-        Numerotentativa ++ // acrescentando mais um numero a tentativa
+        Numerotentativa ++
     };
+}
 
+if(Numerotentativa == 1){
+    alert(`Muito bom, você acertou em ${Numerotentativa} tentativa!!`)
+}
+else {                                                                                                                                              
+    alert(`Muito bom, você acertou em ${Numerotentativa} tentativas!!`)
 }
