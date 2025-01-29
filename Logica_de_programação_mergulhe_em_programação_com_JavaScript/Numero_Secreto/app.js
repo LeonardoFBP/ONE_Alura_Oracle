625b9c61cf97d852b0e657bb66bdd5576b919a81
Leonardo Filipe <leonardo.filipe.trabalho.ti@gmail.com>
2025-01-29 20:49:38 -0300

    Interagindo com campos de texto
    Todo o restante será definido como comentário.

--- Logica_de_programação_mergulhe_em_programação_com_JavaScript/Numero_Secreto/app.js
+++ Logica_de_programação_mergulhe_em_programação_com_JavaScript/Numero_Secreto/app.js
@@ -1,4 +1,4 @@
-let NumeroSecretoMaximo = 100;    // Definindo uma variavel para altera-la dentro do math.random
+/*let NumeroSecretoMaximo = 100;    // Definindo uma variavel para altera-la dentro do math.random
 let NumeroSecreto = parseInt(Math.random() * NumeroSecretoMaximo + 1); //Diminuindo  o acoplamento
 let tentativa;
 let Numerotentativa = 1;
@@ -22,5 +22,9 @@ while (NumeroSecreto!= tentativa){
 let palavraTentativa = Numerotentativa > 1 ? 'tentativas' : 'tentativa';
 
 alert(`Isso ai! Você descobriu o número secreto ${NumeroSecreto} com ${Numerotentativa} ${palavraTentativa}.`);
-
+*/
+let titulo = document.querySelector('h1');  // Declarando variavel para alterar o texto, DOCUMENT seleciona o HTML, QuerrySelector seleciona o local do HTML.
+titulo.innerHTML = 'Jogo do número secreto';  //Para inserir deve usar a variavel com a propriedade.innerHTML e definir.
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número de 1 a 10';
 
