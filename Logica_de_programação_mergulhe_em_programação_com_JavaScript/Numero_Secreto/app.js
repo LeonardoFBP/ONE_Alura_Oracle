9e5c87d739b9d1515dfba589663feef5b5271fdf
Leonardo Filipe <leonardo.filipe.trabalho.ti@gmail.com>
2025-01-29 20:56:52 -0300

    Criando referencia Html e chamando no JS
    Criado a referência da função no HTML (verificarchute() ) e implementei a função no JavaScript

--- Logica_de_programação_mergulhe_em_programação_com_JavaScript/Numero_Secreto/app.js
+++ Logica_de_programação_mergulhe_em_programação_com_JavaScript/Numero_Secreto/app.js
@@ -28,3 +28,7 @@ titulo.innerHTML = 'Jogo do número secreto';  //Para inserir deve usar a variav
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número de 1 a 10';
 
+function verificarChute(){
+    console.log('Foi realizado um clik.')
+} 
+
